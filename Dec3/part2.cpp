cbef57ac94cd618ad12b705624b928ecc4b68b5c
Sabine Randow <sabine.randow@gmail.com>
2021-12-03 19:24:54 +0100

    part 1 day 3 work, not 2 lol

--- Dec3/part2.cpp
+++ Dec3/part2.cpp
@@ -5,57 +5,99 @@
 #include <math.h>       /* pow */
 using namespace std;
 
-int most_common_num(vector<string> origin, int times){
+void most_common_num(vector<string> o2_origin,vector<string> co2_origin, int times){
 
-    cout << origin.size();
-    vector<string> copy;
-    copy.reserve(1000);
-    int bits=0;
-    for (int j = 0; j < origin.size(); j++){
-        if(origin[j].at(11-times)=='1'){
-            bits++;
+    vector<string> o2_copy;
+    int size_o2 = o2_origin.size();
+    o2_copy.reserve(size_o2);
+
+    vector<string> co2_copy;
+    int size_co2 = co2_origin.size();
+   // cout << "sice of origin: " << co2_origin.size() << endl;
+ 
+    co2_copy.reserve(size_co2);
+   // cout << "sice of vector: " << co2_copy.size() << endl;
+    // count number of 1:s at position 11-times
+    int bits_o2=0;
+    for (int j = 0; j < o2_origin.size(); j++){
+        if(o2_origin[j].at(11-times)=='1'){
+            bits_o2++;
+        }
+    }
+    // determine what sign there is the most of
+    char dominant_o2 = '1';
+    if (bits_o2<(o2_origin.size()/2)){
+        dominant_o2 = '0';
+    }
+
+
+    // same for co2
+    int bits_co2=0;
+    for (int j = 0; j < co2_origin.size(); j++){
+        if(co2_origin[j].at(11-times)=='1'){
+            bits_co2++;
         }
     }
 
-    char dominant = '1';
-    if (bits<(origin.size()/2)){
-        dominant = '0';
+    // determine what sign there is the most of
+    char dominant_co2 = '1';
+    if (bits_co2<(co2_origin.size()/2)){
+        dominant_o2 = '0';
     }
 
-    //cout << "dominant bit is " << dominant << endl;
-    for (int i =0; i < origin.size(); i++){
-        string s = origin[i];
-       // cout << "are bits equal? "<<(s[0] == dominant) << endl;
-        if (s[11-times] == dominant){
-           // cout << "will erase" << endl;
-            copy.push_back(origin.at(i));
+    // O2
+    //Gather all vector elements that match dominant_o2 value att locatio 11-times
+    for (int i =0; i < o2_origin.size(); i++){
+        string s = o2_origin[i];
+        if (s[11-times] == dominant_o2){
+            o2_copy.push_back(o2_origin.at(i));
         }
     }
-    copy.shrink_to_fit();
-    //cout << "size is "<< copy.size() <<endl;
-   // cout << "elem 0 is "<< copy.at(0) << endl<<endl;
-    if (copy.size()==1){
-        string value = copy.at(0);
-        uint64_t retunrVal = 0;
-        cout << "the value is: " <<value << endl;
-        for (int i = 0; i < value.length(); i++){
-            cout << "position is: " << value[i] << endl;
-            cout << "would add: " << pow(2,(12-i-1)) << endl;
-            if(value[i] == '1'){
-                cout << "is in if";
-                retunrVal += pow(2,(12-i-1));
+
+    // CO2
+
+    for (int i =0; i < co2_origin.size(); i++){
+        string s = co2_origin[i];
+ //cout <<"s: "<<s <<" i: " << i << "of" << co2_origin.size()<< endl;
+        if (s[11-times] != dominant_co2){ // !!!!!!!!!!!!!!!!!!!!!!!!!!
+        //cout << "will pushback"<< endl;
+            co2_copy.push_back(co2_origin.at(i));
+           // cout << co2_copy.back();        
+        }
+    }
+
+    // shrink vector to its contents
+    o2_copy.shrink_to_fit();
+    co2_copy.shrink_to_fit();
+
+    // start calculating the actual value from binary string
+    // when there is one elem left inte vector<string>
+    if (o2_copy.size()==1){
+        string o2 = o2_copy.at(0);
+        string co2 = co2_copy.at(0);
+        uint64_t returnValO2 = 0;
+        uint64_t returnValCO2 = 0;
+        for (int i = 0; i < o2.length(); i++){
+            if(o2[i] == '1'){
+                returnValO2 += pow(2,(12-i-1));
             }
-            cout << "return is now " << retunrVal << endl;
+            if(co2[i] == '1'){
+                returnValCO2 += pow(2,(12-i-1));
+            }
         }
-        cout << "the result is " << retunrVal;
-        return retunrVal;
+        // write the results to terminal
+        cout <<endl<< "the result of O2 is " << returnValO2<<endl;
+        cout <<endl<< "the result of CO2 is " << returnValCO2<<endl;
+        cout << "The product is: " << returnValO2 * returnValCO2 << endl;
+        return;
     }
-    //cout << "one more time..." << endl;
-    most_common_num(copy, times-1);
-    //return "nothing";
-    //return result;
-}
 
+    cout << co2_copy.at(0) << endl;
+    // will run total of 11 times
+    cout << "one more time.." << endl;
+    most_common_num(o2_copy,co2_copy, times-1);
+    return;
+}
 
 int main () {
 
@@ -90,7 +132,7 @@ int main () {
             bits[0]++;
         }
     }
-    most_common_num(binary_numbers,11);
+    most_common_num(binary_numbers,binary_numbers,11); // 126 for O2
 
 
     return 0;
